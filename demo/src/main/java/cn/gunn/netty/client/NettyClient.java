f444c0557e5d831742e1dddc090bd8518576c671
ganjunhui <317729530@qq.com>
2018-10-14 22:49:06 +0800

    netty client retry connect if connect fail

--- demo/src/main/java/cn/gunn/netty/client/NettyClient.java
+++ demo/src/main/java/cn/gunn/netty/client/NettyClient.java
@@ -6,7 +6,11 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
+import java.util.concurrent.TimeUnit;
+
 public class NettyClient {
+
+    public static final int MAX_RETRY = 5;
 
     public static void main(String[] args) throws InterruptedException {
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
@@ -22,15 +26,40 @@ public class NettyClient {
                     }
                 });
 
-        bootstrap.connect("192.168.2.142", 8000)
+//        bootstrap.connect("192.168.2.142", 8000)
+//                .addListener(future -> {
+//                    if (future.isSuccess()) {
+//                        System.out.println("connect success");
+//                    } else {
+//                        System.out.println("connect falid");
+//                    }
+//                });
+//
+//        Thread.sleep(100000);
+
+        connect(bootstrap, "192.168.2.142", 8000, MAX_RETRY);
+    }
+
+    /**
+     * connet to server,if connect fail,retry it
+     *
+     * @param host  server hostname
+     * @param port  server port
+     * @param retry retry times
+     */
+    private static void connect(Bootstrap bootstrap, String host, int port, int retry) {
+        bootstrap.connect(host, port)
                 .addListener(future -> {
                     if (future.isSuccess()) {
                         System.out.println("connect success");
+                    } else if (retry == 0) {
+                        System.err.println("retry connect " + retry + " times,quit!");
                     } else {
-                        System.out.println("connect falid");
+                        int order = (MAX_RETRY - retry) + 1;
+                        int delay = 1 << order;
+                        bootstrap.config().group().schedule(() -> connect(bootstrap, host, port, retry - 1), delay, TimeUnit.SECONDS);
                     }
                 });
 
-        Thread.sleep(100000);
     }
 }
