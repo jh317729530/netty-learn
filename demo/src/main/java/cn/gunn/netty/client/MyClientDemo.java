01865e463562aa2df84c51863a5eb77bf07c6a69
ganjunhui <junhui_g@gzleihou.cn>
2018-10-19 17:35:26 +0800

    删除无效引用

--- demo/src/main/java/cn/gunn/netty/client/MyClientDemo.java
+++ demo/src/main/java/cn/gunn/netty/client/MyClientDemo.java
@@ -1,11 +1,8 @@
 package cn.gunn.netty.client;
 
-import cn.gunn.netty.handler.FirstClientHandler;
-import cn.gunn.netty.handler.FirstServerHandler;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
